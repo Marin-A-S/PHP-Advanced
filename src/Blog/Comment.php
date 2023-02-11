0b2bab01909ac99bc99cfa55200a31d624361530
Marin-A-S <106108224+Marin-A-S@users.noreply.github.com>
2023-02-11 19:43:34 +0300

    lesson-1 homework

---
+++ src/Blog/Comment.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace src\Blog;
+
+class Comment
+{
+    public function __construct(
+        private int $id,
+        private int $authorId,
+        private int $blogId,
+        private string $text
+    ) {
+    }
+}
